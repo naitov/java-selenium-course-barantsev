0d4454f19b548f848c7d0ec1e880426c8187ba74
Aliaksei Yurchanka <naitaff@gmail.com>
2023-01-16 23:07:29 +0100

    Added test class: GroupModificationTests.java

---
+++ src/test/java/online/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,20 @@
+package online.addressbook.tests;
+
+import lombok.extern.java.Log;
+import online.addressbook.model.GroupData;
+import org.testng.annotations.Test;
+
+@Log
+public class GroupModificationTests extends TestBase {
+
+    @Test
+    public void testGroupModification() {
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectFirstGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("test22", "33", "55"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+        log.info("Modified first group in list");
+    }
+}
