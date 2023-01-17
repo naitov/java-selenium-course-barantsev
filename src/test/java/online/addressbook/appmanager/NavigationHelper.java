f052691a39cb11bfbf438d55d9afe4937783db7a
Aliaksei Yurchanka <naitaff@gmail.com>
2023-01-17 20:19:56 +0100

    Added conditions in NavigationHelper

--- src/test/java/online/addressbook/appmanager/NavigationHelper.java
+++ src/test/java/online/addressbook/appmanager/NavigationHelper.java
@@ -10,15 +10,23 @@ public class NavigationHelper extends HelperBase {
     }
 
     public void gotoGroupPage() {
-
+        if (isElementPresent(By.name("new")) && driver.getCurrentUrl().contains("/group.php")) {
+            return;
+        }
         click(By.xpath("//*[@id='nav']/ul/li[3]/a"));
     }
 
     public void gotoContactPage() {
+        if (isElementPresent(By.name("quickadd"))) {
+            return;
+        }
         click(By.xpath("//*[@id='nav']/ul/li[2]/a"));
     }
 
     public void goToMainPage() {
+        if (isElementPresent(By.id("maintable"))) {
+            return;
+        }
         click(By.xpath("//*[@id='nav']/ul/li[1]/a"));
     }
 }
