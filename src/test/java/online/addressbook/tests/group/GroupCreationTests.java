6506ccd7b0dc10c1bb93df20b4e0795b6b09a5e0
Aliaksei Yurchanka <naitaff@gmail.com>
2023-01-25 19:32:41 +0100

    Parametrized testGroupCreation() in GroupCreationTests.java

--- src/test/java/online/addressbook/tests/group/GroupCreationTests.java
+++ src/test/java/online/addressbook/tests/group/GroupCreationTests.java
@@ -4,7 +4,15 @@ import lombok.extern.java.Log;
 import online.addressbook.model.GroupData;
 import online.addressbook.model.Groups;
 import online.addressbook.tests.TestBase;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -12,11 +20,24 @@ import static org.hamcrest.Matchers.equalTo;
 @Log
 public class GroupCreationTests extends TestBase {
 
-    @Test
-    public void testGroupCreation() {
+    @DataProvider
+    public Iterator<Object[]> validGroups() throws IOException {
+        List<Object[]> list = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/groups.csv"));
+        String line = reader.readLine();
+        while (line != null) {
+            String[] split = line.split(";");
+            list.add(new Object[]{new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+            line = reader.readLine();
+        }
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation(GroupData g) {
         app.goTo().groupPage();
         Groups before = app.group().all();
-        GroupData group = new GroupData().withName("test33");
+        GroupData group = new GroupData().withName(g.getName()).withHeader(g.getHeader()).withFooter(g.getFooter());
         app.group().create(group);
         assertThat(app.group().amount(), equalTo(before.size() + 1));
         Groups after = app.group().all();
